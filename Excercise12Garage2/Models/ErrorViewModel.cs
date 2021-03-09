055f8c83d2538234de06e3c797c61ce4c10ff19f
Oscar Andersson <oscarandersson2@gmail.com>
2021-03-09 14:32:39 +0100

    Add project files.

---
+++ Excercise12Garage2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Excercise12Garage2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
