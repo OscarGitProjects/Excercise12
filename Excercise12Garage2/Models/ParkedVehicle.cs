bf86a5a70066a1cb93ed21a9e9c0981c6048cfc7
Kenneth Okalang <68539411+Atamakit@users.noreply.github.com>
2021-03-10 13:22:04 +0100

    Changes to model

---
+++ Excercise12Garage2/Models/ParkedVehicle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Excercise12Garage2.Models
+{
+    public class ParkedVehicle
+    {
+        public int Id { get; set; }
+
+        public string VehicleType { get; set; }
+        public string RegistrationNumber { get; set; }
+
+        [Required(ErrorMessage = "Please insert a valid color")]
+        [StringLength(20)]
+        [DisplayFormat(NullDisplayText = "Undefined")]
+        [MaxLength(1)]
+        public string Color { get; set; }
+        public string Make { get; set; }
+
+        [StringLength(20)]
+        [DisplayFormat(NullDisplayText = "Undefined")]
+        public string Model { get; set; }
+
+        [Required(ErrorMessage = "Please insert a valid number of wheels")]
+        [DisplayFormat(NullDisplayText = "Undefined")]
+        [MaxLength(1)]
+        [DisplayName("Nr Of Wheels")]
+        public int NumberOfWheels { get; set; }
+
+        [DisplayName("Time Checked Out")]
+        public DateTime CheckIn { get; set; }
+
+       
+
+       
+        
+       
+    }
+
+
+}
+    
+
