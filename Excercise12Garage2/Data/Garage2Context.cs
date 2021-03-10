9bb7bc3a0e4d870ec33b499b8d03210caa580883
Mohammad Saiful Islam <msisaiful@outlook.com>
2021-03-10 11:37:23 +0100

    Adding demo data

---
+++ Excercise12Garage2/Data/Garage2Context.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Excercise12Garage2.Data
+{
+    public class Garage2Context : DbContext
+    {
+        public DbSet<ParkedVehicle> ParkedVehicle { get; set; }
+        public Garage2Context(DbContextOptions<Garage2Context> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ParkedVehicle>().HasData(
+                 new ParkedVehicle
+                 {
+                     Id = 1,
+                     VehicleType = "Sports Car",
+                     RegistrationNumber = "EKY 055",
+                     Colour = "Blue",
+                     Make = "Koenigsegg",
+                     Model = "CCR",
+                     NumberOfWheels = 4,
+                     TimeOfArrival = DateTime.Now.AddDays(-16)
+                 },
+                 new ParkedVehicle
+                 {
+                     Id = 2,
+                     VehicleType = "Trucks",
+                     RegistrationNumber = "KST 810",
+                     Colour = "silver",
+                     Make = "Scania AB",
+                     Model = "2017 Scania R Series",
+                     NumberOfWheels = 8,
+                     TimeOfArrival = DateTime.Now.AddDays(-33)
+                 },
+
+                 new ParkedVehicle
+                 {
+                     Id = 3,
+                     VehicleType = "Bus",
+                     RegistrationNumber = "D 7900",
+                     Colour = "Green",
+                     Make = "Volvo",
+                     Model = "2018 D Series",
+                     NumberOfWheels = 8,
+                     TimeOfArrival = DateTime.Now.AddDays(-20)
+                 },
+
+                 new ParkedVehicle
+                 {
+                     Id = 4,
+                     VehicleType = "Car",
+                     RegistrationNumber = "H 965",
+                     Colour = "Black",
+                     Make = "SAAB",
+                     Model = "Saab 9000",
+                     NumberOfWheels = 4,
+                     TimeOfArrival = DateTime.Now.AddDays(-5)
+                 },
+
+                 new ParkedVehicle
+                 {
+                     Id = 5,
+                     VehicleType = "Motorcycles",
+                     RegistrationNumber = "H 345",
+                     Colour = "Ash",
+                     Make = "Husqvarna",
+                     Model = "Husqvarna 2020",
+                     NumberOfWheels = 2,
+                     TimeOfArrival = DateTime.Now.AddDays(-4)
+                 },
+                 new ParkedVehicle
+                 {
+                     Id = 6,
+                     VehicleType = "Ship",
+                     RegistrationNumber = "IMO 8814275",
+                     Colour = "White",
+                     Make = "SSPA Sweden AB",
+                     Model = "SSPA 2019",
+                     NumberOfWheels = 2,
+                     TimeOfArrival = DateTime.Now.AddDays(-7)
+                 },
+                 new ParkedVehicle
+                 {
+                     Id = 7,
+                     VehicleType = "Airplane",
+                     RegistrationNumber = "A330-300",
+                     Colour = "Yellow",
+                     Make = "SAS",
+                     Model = "Airbus A330",
+                     NumberOfWheels = 3,
+                     TimeOfArrival = DateTime.Now.AddDays(-11)
+                 }
+
+
+                );
+        }
+
+
+    }
+}
