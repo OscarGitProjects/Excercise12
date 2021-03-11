f3714c5518fb5dc9813c293a91eab63546ce0d0a
Oscar Andersson <oscarandersson2@gmail.com>
2021-03-11 17:34:07 +0100

    Remover unused controller and views. Some updates

--- Excercise12Garage2/Startup.cs
+++ Excercise12Garage2/Startup.cs
@@ -55,7 +55,7 @@ namespace Excercise12Garage2
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Vehicles}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
